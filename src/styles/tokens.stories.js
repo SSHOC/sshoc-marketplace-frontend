65fdcb15a50c9e44f0e2036e273775afeeeee2e5
Stefan Probst <stefanprobst@kolabnow.com>
2019-12-10 12:00:47 +0100

    test: add color palette to storybook

---
+++ src/styles/tokens.stories.js
@@ -0,0 +1,35 @@
+import css from '@styled-system/css'
+import 'styled-components/macro'
+import React from 'react'
+import Box from '../elements/Box/Box'
+import FlexList from '../elements/FlexList/FlexList'
+import Stack from '../elements/Stack/Stack'
+import Text from '../elements/Text/Text'
+
+export default {
+  title: 'Tokens|Colors',
+}
+
+export const palette = () => (
+  <FlexList>
+    <li>
+      <Stack css={css({ alignItems: 'center', mx: 2 })}>
+        <Text variant="small">primary</Text>
+        <Box css={css({ width: 50, height: 50, bg: 'primary', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'primaryActive', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'primaryHover', my: 1 })} />
+      </Stack>
+    </li>
+    <li>
+      <Stack css={css({ alignItems: 'center', mx: 2 })}>
+        <Text variant="small">grey</Text>
+        <Box css={css({ width: 50, height: 50, bg: 'black', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'muted', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'border', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'subtle', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'subtler', my: 1 })} />
+        <Box css={css({ width: 50, height: 50, bg: 'subtleest', my: 1 })} />
+      </Stack>
+    </li>
+  </FlexList>
+)
