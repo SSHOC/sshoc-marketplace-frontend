771e90644c205b3d3fc6169908f985f76693eb81
Stefan Probst <stefanprobst@kolabnow.com>
2019-12-11 12:51:59 +0100

    fix: add website and email to contributors in item details view

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -6,9 +6,53 @@ import Centered from '../../elements/Centered/Centered'
 import Flex from '../../elements/Flex/Flex'
 import Heading from '../../elements/Heading/Heading'
 import Icon from '../../elements/Icon/Icon'
+import Link from '../../elements/Link/Link'
 import Spinner from '../../elements/Spinner/Spinner'
 import Text from '../../elements/Text/Text'
 import { REQUEST_STATUS } from '../../store/constants'
+import RelatedItems from '../RelatedItems/RelatedItems'
+
+const ItemContributors = ({ contributors }) => {
+  if (!contributors || !contributors.length) {
+    return null
+  }
+
+  return (
+    <Text css={css({ color: 'grey.900' })} variant="small">
+      <span css={css({ color: 'grey.800' })}>Contributors: </span>
+      {contributors.map((contributor, i) => (
+        <>
+          {contributor.actor.website ? (
+            <span>
+              <Link css={css({ color: 'text' })} to={contributor.actor.website}>
+                {contributor.actor.name}
+              </Link>
+            </span>
+          ) : (
+            <span>{contributor.actor.name}</span>
+          )}
+          {contributor.actor.email ? (
+            <>
+              {' '}
+              <span>
+                (
+                <Link
+                  css={css({ color: 'text' })}
+                  to={`mailto:${contributor.actor.email}`}
+                >
+                  {contributor.actor.email}
+                </Link>
+                )
+              </span>
+            </>
+          ) : null}
+
+          {i < contributors.length - 1 && ', '}
+        </>
+      ))}
+    </Text>
+  )
+}
 
 const ItemDetails = ({ request, resource }) => {
   if (resource) {
@@ -26,12 +70,7 @@ const ItemDetails = ({ request, resource }) => {
               </span>
             </Heading>
             <Box>
-              <Text css={css({ color: 'grey.900' })} variant="small">
-                <span css={css({ color: 'grey.800' })}>Contributors: </span>
-                {(resource.contributors || [])
-                  .map(contributor => contributor.actor.name)
-                  .join(', ')}
-              </Text>
+              <ItemContributors contributors={resource.contributors} />
               <Text css={css({ color: 'grey.400' })} variant="small">
                 <span>More Metadata: </span>
                 What goes here?
