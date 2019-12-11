b6e3d0865ebd5514ee4e819c63f6971be59133ce
Stefan Probst <stefanprobst@kolabnow.com>
2019-12-11 13:52:40 +0100

    fix: allow ul as grid

--- src/elements/Grid/Grid.js
+++ src/elements/Grid/Grid.js
@@ -1,9 +1,15 @@
+import css from '@styled-system/css'
 import styled from 'styled-components/macro'
 import Box from '../Box/Box'
 
-const Grid = styled(Box)(props => ({
-  display: 'grid',
-  gridGap: props.gap,
-}))
+const Grid = styled(Box)(props =>
+  css({
+    display: 'grid',
+    gridGap: props.gap,
+    listStyle: 'none',
+    m: 0,
+    p: 0,
+  })
+)
 
 export default Grid
