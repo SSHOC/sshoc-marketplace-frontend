7438e290ed0310a881ccb96c248368e2dfc9297b
Stefan Probst <stefanprobst@kolabnow.com>
2020-01-16 19:10:47 +0100

    test: fix e2e test

--- cypress/e2e/App.test.js
+++ cypress/e2e/App.test.js
@@ -1,6 +1,6 @@
 describe('App', () => {
   it('is just a placeholder', () => {
     cy.visit('/')
-    cy.contains('SSHOC Marketplace')
+    cy.contains('Social Sciences & Humanities Open Marketplace')
   })
 })
