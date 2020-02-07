b5e85c032fc3323ecfb61d88ab64c9c2867f7c16
Stefan Probst <stefanprobst@kolabnow.com>
2020-02-08 00:10:53 +0100

    chore: move router into root component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import About from '../../pages/About/About'
 import Contact from '../../pages/Contact/Contact'
 import Dataset from '../../pages/Dataset/Dataset'
@@ -13,23 +13,21 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 import Page from '../Page/Page'
 
 const App = () => (
-  <Router>
-    <Page>
-      <ErrorBoundary>
-        <Switch>
-          <Route component={Home} exact path="/" />
-          <Route component={About} path="/about" />
-          <Route component={Contact} path="/contact" />
-          <Route component={Dataset} path="/datasets/:id" />
-          <Route component={PrivacyPolicy} path="/privacy-policy" />
-          <Route component={Search} path="/search" />
-          <Route component={Tool} path="/tools/:id" />
-          <Route component={TrainingMaterial} path="/training-materials/:id" />
-          <Route component={NotFound} />
-        </Switch>
-      </ErrorBoundary>
-    </Page>
-  </Router>
+  <Page>
+    <ErrorBoundary>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={About} path="/about" />
+        <Route component={Contact} path="/contact" />
+        <Route component={Dataset} path="/datasets/:id" />
+        <Route component={PrivacyPolicy} path="/privacy-policy" />
+        <Route component={Search} path="/search" />
+        <Route component={Tool} path="/tools/:id" />
+        <Route component={TrainingMaterial} path="/training-materials/:id" />
+        <Route component={NotFound} />
+      </Switch>
+    </ErrorBoundary>
+  </Page>
 )
 
 export default App
--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { configureStore } from '../../store/configureStore'
 import { GlobalStyles, ThemeProvider } from '../../styles'
 import App from '../App/App'
@@ -10,7 +11,9 @@ const Root = () => (
   <Provider store={store}>
     <ThemeProvider>
       <GlobalStyles />
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ThemeProvider>
   </Provider>
 )
