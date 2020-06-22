31f2af490b359c753b1847b7be6a485c67243531
Stefan Probst <stefanprobst@kolabnow.com>
2020-06-22 07:16:12 +0200

    chore: make browse wip screen less ugly

--- src/components/BrowseScreen/BrowseScreen.js
+++ src/components/BrowseScreen/BrowseScreen.js
@@ -6,11 +6,11 @@ import Heading from '../../elements/Heading/Heading'
 import Text from '../../elements/Text/Text'
 
 const BrowseScreen = () => (
-  <Container css={css({ my: 6 })} variant="narrow">
+  <Container css={css({ my: 6, width: '100%', px: 0 })}>
     <Heading as="h1" css={css({ fontSize: 26, mb: 4 })} variant="h2">
       Browse
     </Heading>
-    <Text>Sorry, not done yet.</Text>
+    <Text>Sorry, this screen is still work in progress.</Text>
   </Container>
 )
 
