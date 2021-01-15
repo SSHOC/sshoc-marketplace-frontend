82e17e8cbd717e1b8e80fc46f3d648737f924e4d
Stefan Probst <stefanprobst@kolabnow.com>
2021-01-15 12:41:28 +0100

    fix: last updated at => on

--- src/modules/ui/LastUpdatedAt.tsx
+++ src/modules/ui/LastUpdatedAt.tsx
@@ -3,7 +3,7 @@ import { formatDate } from '@/utils/formatDate'
 export default function LastUpdatedAt({ date }: { date: string }): JSX.Element {
   return (
     <div className="text-sm">
-      Last updated at <time dateTime={date}>{formatDate(date)}</time>
+      Last updated on <time dateTime={date}>{formatDate(date)}</time>
     </div>
   )
 }
