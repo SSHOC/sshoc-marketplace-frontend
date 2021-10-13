e5f601f56bee5214e85512b3c3f7d8f3ace0e543
Stefan Probst <stefanprobst@kolabnow.com>
2021-10-13 13:07:56 +0200

    feat: add suggest new concept button

--- src/components/item/PropertiesFormSection/PropertiesFormSection.tsx
+++ src/components/item/PropertiesFormSection/PropertiesFormSection.tsx
@@ -1,12 +1,25 @@
+import { Dialog } from '@reach/dialog'
+import { camelCase } from 'change-case'
 import { Fragment, useEffect, useMemo, useState } from 'react'
 import type { QueryObserverResult } from 'react-query'
+import { useQueryClient } from 'react-query'
 
 import type {
+  ConceptCore,
   PaginatedPropertyTypes,
   PropertyTypeDto,
   SearchConcept,
+  VocabularyBasicDto,
 } from '@/api/sshoc'
-import { useGetPropertyTypes, useSearchConcepts } from '@/api/sshoc'
+import {
+  useCreateConcept,
+  useGetPropertyTypes,
+  useSearchConcepts,
+} from '@/api/sshoc'
+import { Button } from '@/elements/Button/Button'
+import { Icon } from '@/elements/Icon/Icon'
+import { Svg as CloseIcon } from '@/elements/icons/small/cross.svg'
+import { useToast } from '@/elements/Toast/useToast'
 import { useDebouncedState } from '@/lib/hooks/useDebouncedState'
 import { useAuth } from '@/modules/auth/AuthContext'
 import { FormComboBox } from '@/modules/form/components/FormComboBox/FormComboBox'
@@ -17,6 +30,7 @@ import { FormRecords } from '@/modules/form/components/FormRecords/FormRecords'
 import { FormSection } from '@/modules/form/components/FormSection/FormSection'
 import { FormSelect } from '@/modules/form/components/FormSelect/FormSelect'
 import { FormTextField } from '@/modules/form/components/FormTextField/FormTextField'
+import { Form } from '@/modules/form/Form'
 import { FormField } from '@/modules/form/FormField'
 import { FormFieldArray } from '@/modules/form/FormFieldArray'
 import { FormFieldCondition } from '@/modules/form/FormFieldCondition'
@@ -66,6 +80,21 @@ export function PropertiesFormSection(
 
     return map
   }, [propertyTypes.data])
+
+  const [showSuggestConceptDialog, setShowSuggestConceptDialog] = useState(
+    false,
+  )
+  const [propertyTypeIdForDialog, setPropertyTypeIdForDialog] = useState<
+    string | null
+  >(null)
+  function openSuggestConceptDialog(propertyTypeId: string) {
+    setPropertyTypeIdForDialog(propertyTypeId)
+    setShowSuggestConceptDialog(true)
+  }
+  function closeSuggestConceptDialog() {
+    setPropertyTypeIdForDialog(null)
+    setShowSuggestConceptDialog(false)
+  }
 
   return (
     <FormSection title={'Properties'}>
@@ -122,14 +151,24 @@ export function PropertiesFormSection(
                       >
                         {(id: string) => {
                           return (
-                            <PropertyConceptComboBox
-                              name={`${name}.concept.uri`}
-                              parentName={name}
-                              label={'Concept'}
-                              propertyTypeId={id}
-                              initialValues={props.initialValues}
-                              index={index}
-                            />
+                            <Fragment>
+                              <PropertyConceptComboBox
+                                name={`${name}.concept.uri`}
+                                parentName={name}
+                                label={'Concept'}
+                                propertyTypeId={id}
+                                initialValues={props.initialValues}
+                                index={index}
+                              />
+                              <button
+                                className="text-ui-base text-primary-750 hover:text-secondary-600"
+                                onClick={() => {
+                                  openSuggestConceptDialog(id)
+                                }}
+                              >
+                                Suggest new concept
+                              </button>
+                            </Fragment>
                           )
                         }}
                       </FormFieldCondition>
@@ -166,6 +205,15 @@ export function PropertiesFormSection(
           )
         }}
       </FormFieldArray>
+      <SuggestConceptDialog
+        isOpen={showSuggestConceptDialog}
+        onDismiss={closeSuggestConceptDialog}
+        propertyType={
+          propertyTypeIdForDialog != null
+            ? propertyTypesById[propertyTypeIdForDialog]
+            : undefined
+        }
+      />
     </FormSection>
   )
 }
@@ -304,4 +352,194 @@ function PropertyConceptComboBox(
       </FormFieldCondition>
     </Fragment>
   )
+}
+
+interface SuggestConceptDialogProps {
+  isOpen: boolean
+  onDismiss: () => void
+  propertyType?: PropertyTypeDto
+}
+
+/**
+ * Dialog to suggest new candidate concept to be included in a vocabulary.
+ */
+function SuggestConceptDialog(props: SuggestConceptDialogProps) {
+  const createConcept = useCreateConcept()
+  const auth = useAuth()
+  const queryClient = useQueryClient()
+  const toast = useToast()
+
+  function onSubmit(unsanitized: ConceptFormValues) {
+    if (auth.session?.accessToken === undefined) {
+      toast.error('Authentication required.')
+      return Promise.reject()
+    }
+
+    const { vocabularyId, ...values } = sanitizeConceptFormValues(unsanitized)
+
+    return createConcept.mutateAsync(
+      [
+        { 'vocabulary-code': vocabularyId },
+        { candidate: true },
+        values,
+        { token: auth.session.accessToken },
+      ],
+      {
+        onSuccess() {
+          queryClient.invalidateQueries(['getConcepts'])
+          toast.success('Sucessfully suggested concept.')
+        },
+        onError() {
+          toast.error('Failed to suggest concept.')
+        },
+        onSettled() {
+          props.onDismiss()
+        },
+      },
+    )
+  }
+
+  if (props.propertyType == null) {
+    return null
+  }
+
+  return (
+    <Dialog
+      isOpen={props.isOpen}
+      onDismiss={props.onDismiss}
+      className="flex flex-col w-full max-w-screen-lg px-32 py-16 mx-auto bg-white rounded shadow-lg"
+      style={{ width: '60vw', marginTop: '10vh', marginBottom: '10vh' }}
+      aria-label="Suggest new candidate concept"
+    >
+      <button
+        onClick={props.onDismiss}
+        className="self-end"
+        aria-label="Close dialog"
+      >
+        <Icon icon={CloseIcon} className="" />
+      </button>
+      <section className="flex flex-col space-y-6">
+        <h2 className="text-2xl font-medium">Suggest new concept</h2>
+        {/* this form is rendered in a portal, so it's valid html, even though it's a <form> "nested" in another <form>. */}
+        <CreateConceptForm
+          onDismiss={props.onDismiss}
+          onSubmit={onSubmit}
+          isLoading={createConcept.isLoading}
+          buttonLabel="Suggest"
+          allowedVocabularies={props.propertyType.allowedVocabularies!}
+        />
+      </section>
+    </Dialog>
+  )
+}
+
+type ConceptFormValues = ConceptCore & { vocabularyId: string }
+
+interface CreateConceptFormProps {
+  onDismiss: () => void
+  initialValues?: ConceptFormValues
+  onSubmit: (actor: ConceptFormValues) => void
+  isLoading: boolean
+  buttonLabel: string
+  allowedVocabularies: Array<VocabularyBasicDto>
+}
+
+/**
+ * Create concept.
+ */
+function CreateConceptForm(props: CreateConceptFormProps) {
+  const { initialValues, onSubmit, isLoading, buttonLabel } = props
+
+  function onValidate(values: Partial<ConceptFormValues>) {
+    const errors: Partial<Record<keyof typeof values, any>> = {}
+
+    return errors
+  }
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      validate={onValidate}
+      initialValues={initialValues}
+    >
+      {({ handleSubmit, submitting, pristine, invalid }) => {
+        return (
+          <form
+            noValidate
+            className="flex flex-col space-y-6"
+            onSubmit={handleSubmit}
+          >
+            <FormSelect
+              isRequired
+              name="vocabularyId"
+              label="Vocabulary"
+              items={props.allowedVocabularies ?? []}
+              variant="form"
+              style={{ flex: 1 }}
+            >
+              {(item) => {
+                /** Unfortunately we have concepts with empty string labels on -dev instance. */
+                // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+                const label = item.label || item.code
+                return (
+                  <FormSelect.Item key={item.code} textValue={label}>
+                    {label}
+                  </FormSelect.Item>
+                )
+              }}
+            </FormSelect>
+            <FormTextField
+              name="label"
+              label="Label"
+              isRequired
+              variant="form"
+              style={{ flex: 1 }}
+            />
+            <FormTextField
+              name="notation"
+              label="Notation"
+              variant="form"
+              style={{ flex: 1 }}
+            />
+            <FormTextField
+              name="definition"
+              label="Definition"
+              variant="form"
+              style={{ flex: 1 }}
+            />
+            {/* <FormTextField
+              name="uri"
+              label="URI"
+              variant="form"
+              style={{ flex: 1 }}
+            /> */}
+            {/* TODO: Related Concepts */}
+            <div className="flex justify-end space-x-12">
+              <Button variant="link" onPress={props.onDismiss}>
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="gradient"
+                isDisabled={submitting || isLoading}
+              >
+                {buttonLabel}
+              </Button>
+            </div>
+          </form>
+        )
+      }}
+    </Form>
+  )
+}
+
+function sanitizeConceptFormValues(unsanitizedValues: ConceptFormValues) {
+  const values = {
+    ...unsanitizedValues,
+    /** Auto-generate concept id from label. */
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    code: camelCase(unsanitizedValues.label!),
+  }
+
+  return values
 }
