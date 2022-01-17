b11a58780b0a740cb2444d5cbd0f44840cb60512
Stefan Probst <stefanprobst@posteo.de>
2022-01-17 17:22:59 +0100

    fix: remove sign-in block

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -4,13 +4,5 @@ import SignUpScreen from '@/screens/auth/SignUpScreen'
  * Sign up page.
  */
 export default function SignUpPage(): JSX.Element | null {
-  /* Disable login on production deploy. */
-  if (
-    process.env.NEXT_PUBLIC_SSHOC_BASE_URL ===
-    'https://marketplace.sshopencloud.eu'
-  ) {
-    return null
-  }
-
   return <SignUpScreen />
 }
