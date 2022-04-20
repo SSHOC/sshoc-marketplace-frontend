f6b8ffe398d1c7ef707459fc062b673583f7aa63
Stefan Probst <stefanprobst@posteo.de>
2022-04-20 15:13:55 +0200

    fix: hide loading spinner on 404 error on item history page

--- src/components/item-history/TrainingMaterialHistorySearchResults.tsx
+++ src/components/item-history/TrainingMaterialHistorySearchResults.tsx
@@ -1,6 +1,8 @@
 import { ItemHistorySearchResults } from '@/components/item-history/ItemHistorySearchResults'
 import type { TrainingMaterial } from '@/data/sshoc/api/training-material'
 import { useTrainingMaterialHistory } from '@/data/sshoc/hooks/training-material'
+import { isNotFoundError } from '@/data/sshoc/utils/isNotFoundError'
+import type { QueryMetadata } from '@/lib/core/query/types'
 
 export interface TrainingMaterialHistorySearchResultsProps {
   persistentId: TrainingMaterial['persistentId']
@@ -11,7 +13,16 @@ export function TrainingMaterialHistorySearchResults(
 ): JSX.Element {
   const { persistentId } = props
 
-  const items = useTrainingMaterialHistory({ persistentId })
+  const meta: QueryMetadata = {
+    messages: {
+      error(error) {
+        if (isNotFoundError(error)) return false
+        return undefined
+      },
+    },
+  }
+
+  const items = useTrainingMaterialHistory({ persistentId }, undefined, { meta })
 
   return <ItemHistorySearchResults items={items} />
 }
--- src/components/item-history/WorkflowHistorySearchResults.tsx
+++ src/components/item-history/WorkflowHistorySearchResults.tsx
@@ -1,6 +1,8 @@
 import { ItemHistorySearchResults } from '@/components/item-history/ItemHistorySearchResults'
 import type { Workflow } from '@/data/sshoc/api/workflow'
 import { useWorkflowHistory } from '@/data/sshoc/hooks/workflow'
+import { isNotFoundError } from '@/data/sshoc/utils/isNotFoundError'
+import type { QueryMetadata } from '@/lib/core/query/types'
 
 export interface WorkflowHistorySearchResultsProps {
   persistentId: Workflow['persistentId']
@@ -11,7 +13,16 @@ export function WorkflowHistorySearchResults(
 ): JSX.Element {
   const { persistentId } = props
 
-  const items = useWorkflowHistory({ persistentId })
+  const meta: QueryMetadata = {
+    messages: {
+      error(error) {
+        if (isNotFoundError(error)) return false
+        return undefined
+      },
+    },
+  }
+
+  const items = useWorkflowHistory({ persistentId }, undefined, { meta })
 
   return <ItemHistorySearchResults items={items} />
 }
