a2e358f1ed98925d9fbc66fcd812bd0a82d68511
Stefan Probst <stefanprobst@posteo.de>
2024-03-19 19:12:07 +0100

    fix: hide item citation

--- src/components/item/ItemCitation.tsx
+++ src/components/item/ItemCitation.tsx
@@ -1,59 +1,62 @@
-import styles from '@/components/item/ItemCitation.module.css'
+// import styles from '@/components/item/ItemCitation.module.css'
 import type { Item } from '@/data/sshoc/api/item'
-import { itemRoutes as routes } from '@/lib/core/navigation/item-routes'
-import { createSiteUrl, isNonEmptyString } from '@/lib/utils'
+// import { itemRoutes as routes } from '@/lib/core/navigation/item-routes'
+// import { createSiteUrl, isNonEmptyString } from '@/lib/utils'
 
 export interface ItemCitationProps {
   item: Item
 }
 
-export function ItemCitation(props: ItemCitationProps): JSX.Element {
-  const { item } = props
+export function ItemCitation(_props: ItemCitationProps): JSX.Element | null {
+  /** @see https://github.com/SSHOC/sshoc-marketplace-frontend/issues/182 */
+  return null
 
-  const citation = useCitation(item)
+  // const { item } = props
 
-  return (
-    <small className={styles['container']}>
-      <span>Cite as: </span>
-      <span>{citation}</span>
-    </small>
-  )
+  // const citation = useCitation(item)
+
+  // return (
+  //   <small className={styles['container']}>
+  //     <span>Cite as: </span>
+  //     <span>{citation}</span>
+  //   </small>
+  // )
 }
 
-function useCitation(item: Item): string {
-  const title = item.label
-  const version = item.version
-  const url = createSiteUrl(routes.ItemPage(item.category)({ persistentId: item.persistentId }))
-  const year = new Date(item.lastInfoUpdate).getUTCFullYear()
-  const authors = item.contributors
-    .filter((contributor) => {
-      return contributor.role.code === 'author'
-    })
-    .map((contributor) => {
-      return contributor.actor.name
-    })
+// function useCitation(item: Item): string {
+//   const title = item.label
+//   const version = item.version
+//   const url = createSiteUrl(routes.ItemPage(item.category)({ persistentId: item.persistentId }))
+//   const year = new Date(item.lastInfoUpdate).getUTCFullYear()
+//   const authors = item.contributors
+//     .filter((contributor) => {
+//       return contributor.role.code === 'author'
+//     })
+//     .map((contributor) => {
+//       return contributor.actor.name
+//     })
 
-  let citation = ''
+//   let citation = ''
 
-  if (authors.length > 0) {
-    citation += `${authors.join(', ')} (${year}): `
-  }
+//   if (authors.length > 0) {
+//     citation += `${authors.join(', ')} (${year}): `
+//   }
 
-  citation += title
+//   citation += title
 
-  if (!/[!?]$/.test(title)) {
-    citation += '. '
-  } else {
-    citation += ' '
-  }
+//   if (!/[!?]$/.test(title)) {
+//     citation += '. '
+//   } else {
+//     citation += ' '
+//   }
 
-  if (isNonEmptyString(version)) {
-    citation += `Version ${item.version} `
-  }
+//   if (isNonEmptyString(version)) {
+//     citation += `Version ${item.version} `
+//   }
 
-  citation += `Retrieved ${Intl.DateTimeFormat('en', { dateStyle: 'medium' }).format(
-    new Date(),
-  )} from ${url}`
+//   citation += `Retrieved ${Intl.DateTimeFormat('en', { dateStyle: 'medium' }).format(
+//     new Date(),
+//   )} from ${url}`
 
-  return citation
-}
+//   return citation
+// }
