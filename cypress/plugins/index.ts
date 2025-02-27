760813e5c413742416e41afa5a4f7b895a23d4c3
Stefan Probst <stefanprobst@posteo.de>
2025-02-27 13:25:56 +0100

    chore: remove cypress

--- cypress/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import { baseUrl } from '~/config/site.config'
-
-const plugin: Cypress.PluginConfig = function plugin(on, config): Cypress.ConfigOptions {
-  config.baseUrl = baseUrl
-
-  return config
-}
-
-export default plugin
