6a640dab99164e3bf96be8304c8235c391c15c43
Stefan Probst <stefanprobst@posteo.de>
2025-02-28 22:13:10 +0100

    refactor: remove custom route manifest generation

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-/* @ts-expect-error Missing module declaration. */
 import createBundleAnalyzer from '@next/bundle-analyzer'
 import { log } from '@stefanprobst/log'
-import { RouteManifestPlugin } from '@stefanprobst/next-route-manifest'
 import createSvgPlugin from '@stefanprobst/next-svg'
-import prettierOptions from '@stefanprobst/prettier-config'
 import withToc from '@stefanprobst/rehype-extract-toc'
 import withTocExport from '@stefanprobst/rehype-extract-toc/mdx'
 import withHeadingFragmentLinks from '@stefanprobst/rehype-fragment-links'
@@ -34,7 +31,6 @@ import { defaultLocale, locales } from './config/i18n.config.mjs'
 /** @typedef {import('@mdx-js/loader').Options} MdxOptions */
 /** @typedef {import('@stefanprobst/remark-mdx-page').Options} MdxPageOptions */
 /** @typedef {import('hast').Element} HastElement */
-/** @typedef {import('@stefanprobst/next-route-manifest').Options} RouteManifestConfig */
 
 // TODO:
 const _ContentSecurityPolicy = `
@@ -43,14 +39,6 @@ const _ContentSecurityPolicy = `
   media-src *;
   script-src 'self' matomo.acdh.oeaw.ac.at;
 `
-
-/** @type {RouteManifestConfig} */
-export const routeManifestConfig = {
-  outputFolder: path.join(process.cwd(), 'src', 'lib', 'core', 'navigation'),
-  pagesFolder: path.join(process.cwd(), 'src', 'pages'),
-  pageExtensions: ['page.tsx', 'page.template.tsx'],
-  prettierOptions,
-}
 
 /** @type {NextConfig} */
 const config = {
@@ -163,11 +151,6 @@ const config = {
       use: [{ loader: '@stefanprobst/val-loader' }],
       exclude: /node_modules/,
     })
-
-    /** Auto-generate route manifest. */
-    if (!context.isServer) {
-      config.plugins?.push(new RouteManifestPlugin(routeManifestConfig))
-    }
 
     /** @type {(heading: HastElement, id: string) => Array<HastElement>} */
     function createPermalink(headingElement, id) {
