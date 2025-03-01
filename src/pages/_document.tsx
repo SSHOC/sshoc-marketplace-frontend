6db988f40f4992bca7837abfc9305e8cfc8f13dd
Stefan Probst <stefanprobst@posteo.de>
2025-03-01 18:29:29 +0100

    chore: remove unused theme script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { InitialThemeScript } from '@stefanprobst/next-theme'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 import { PreloadData } from '@/lib/core/app/PreloadData'
@@ -9,14 +8,7 @@ export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        /> */}
         <PreloadData />
-        <InitialThemeScript />
         {isNonEmptyString(googleSiteId) ? (
           <meta name="google-site-verification" content={googleSiteId} />
         ) : null}
