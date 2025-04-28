c6ca83dba4535d2731e5e46c179a09c4b8719cc7
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2025-04-28 13:48:44 +0000

    refactor: update homescreen sections
    reorder sections
    remove subsections
    add section header components
    adjust labels

--- src/components/home/LastUpdatedItems.tsx
+++ src/components/home/LastUpdatedItems.tsx
@@ -4,14 +4,14 @@ import { Fragment } from 'react'
 import { ItemPreview } from '@/components/common/ItemPreview'
 import { SectionTitle } from '@/components/common/SectionTitle'
 import css from '@/components/home/LastUpdatedItems.module.css'
-import { SubSectionHeader } from '@/components/home/SubSectionHeader'
-import { SubSectionHeaderLink } from '@/components/home/SubSectionHeaderLink'
-import { SubSectionTitle } from '@/components/home/SubSectionTitle'
 import { useItemSearch } from '@/data/sshoc/hooks/item'
 import { useI18n } from '@/lib/core/i18n/useI18n'
 import { Centered } from '@/lib/core/ui/Centered/Centered'
 import { LoadingIndicator } from '@/lib/core/ui/LoadingIndicator/LoadingIndicator'
 import { maxLastAddedItems } from '~/config/sshoc.config'
+
+import { SectionHeader } from '../common/SectionHeader'
+import { SectionHeaderLink } from './SectionHeaderLink'
 
 export function LastUpdatedItems(): JSX.Element {
   const { t } = useI18n<'common'>()
@@ -35,16 +35,13 @@ export function LastUpdatedItems(): JSX.Element {
 
   return (
     <section className={css['container']}>
-      <SectionTitle>{t(['common', 'home', 'last-updated'])}</SectionTitle>
+      <SectionHeader>
+        <SectionTitle>{t(['common', 'home', 'see-what-is-new'])}</SectionTitle>
+        <SectionHeaderLink href={`/search?${createUrlSearchParams({ order: ['modified-on'] })}`}>
+          {t(['common', 'see-all'])}
+        </SectionHeaderLink>
+      </SectionHeader>
       <section className={css['section']}>
-        <SubSectionHeader>
-          <SubSectionTitle>{t(['common', 'home', 'see-what-is-new'])}</SubSectionTitle>
-          <SubSectionHeaderLink
-            href={`/search?${createUrlSearchParams({ order: ['modified-on'] })}`}
-          >
-            {t(['common', 'see-all'])}
-          </SubSectionHeaderLink>
-        </SubSectionHeader>
         <ul role="list" className={css['items']}>
           {lastUpdatedItems.map((item) => {
             return (
