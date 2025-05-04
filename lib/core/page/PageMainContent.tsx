c5a47dffd29050d0df890ad5cf1cd7bffa978108
Stefan Probst <stefanprobst@posteo.de>
2025-05-04 11:54:23 +0200

    fix: remove page load indicator

--- lib/core/page/PageMainContent.tsx
+++ lib/core/page/PageMainContent.tsx
@@ -1,21 +1,14 @@
 import type { ReactNode } from "react";
 
 import css from "@/lib/core/page/PageMainContent.module.css";
-import { usePage } from "@/lib/core/page/PageProvider";
 
 export interface PageMainContentProps {
 	children?: ReactNode;
 }
 
 export function PageMainContent(props: PageMainContentProps): ReactNode {
-	const { pageLoadingIndicator, skipToMainContent } = usePage();
-
 	return (
-		<main
-			className={css["container"]}
-			{...pageLoadingIndicator.regionProps}
-			{...skipToMainContent.targetProps}
-		>
+		<main className={css["container"]} id="main-content" tabIndex={-1}>
 			{props.children}
 		</main>
 	);
--- lib/core/page/SkipLink.tsx
+++ lib/core/page/SkipLink.tsx
@@ -1,17 +1,23 @@
 import type { ReactNode } from "react";
 
 import { Link } from "@/components/common/Link";
-import { usePage } from "@/lib/core/page/PageProvider";
 
 export interface SkipLinkProps {
 	children?: ReactNode;
 }
 
 export function SkipLink(props: SkipLinkProps): ReactNode {
-	const { skipToMainContent } = usePage();
+	const id = "main-content";
+
+	function moveFocus() {
+		/**
+		 * Fragment identifier links do not move focus to the target in Firefox.
+		 */
+		document.getElementById(id)?.focus();
+	}
 
 	return (
-		<Link {...skipToMainContent.linkProps} variant="skip-link">
+		<Link href={"#" + id} onClick={moveFocus} variant="skip-link">
 			{props.children}
 		</Link>
 	);
