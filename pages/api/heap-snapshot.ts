2adf472ea0f0e19ad2d50fa19b32f6f180e9d7b8
Stefan Probst <stefanprobst@posteo.de>
2025-06-20 13:37:01 +0200

    debug: add api route to generate heap snapshot

---
+++ pages/api/heap-snapshot.ts
@@ -0,0 +1,61 @@
+import { createReadStream, statSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { basename, join } from "node:path";
+import * as v8 from "node:v8";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { isNonEmptyString, log } from "@/lib/utils";
+
+export default async function handler(
+	request: NextApiRequest,
+	response: NextApiResponse,
+): Promise<void> {
+	if (request.method?.toLowerCase() !== "get") {
+		response.status(405).end();
+		return;
+	}
+
+	const secret = process.env.DEBUG_TOKEN;
+
+	if (!isNonEmptyString(secret)) {
+		response.status(500).end();
+		return;
+	}
+
+	const token = request.headers.authorization;
+
+	if (!isNonEmptyString(token)) {
+		response.status(401).end();
+		return;
+	}
+
+	if (token !== secret) {
+		response.status(403).end();
+		return;
+	}
+
+	const tempDir = tmpdir();
+	const filepath = join(tempDir, `${new Date().toISOString()}.heapsnapshot`);
+
+	const snapshotPath = v8.writeHeapSnapshot(filepath);
+
+	if (!snapshotPath) {
+		response.status(500).end();
+		return;
+	}
+
+	const stream = createReadStream(snapshotPath);
+	const { size } = statSync(snapshotPath);
+
+	response.setHeader("Content-Type", "application/octet-stream");
+	response.setHeader("Content-Disposition", `attachment; filename="${basename(snapshotPath)}"`);
+	response.setHeader("Content-Length", size);
+
+	stream.on("error", (error) => {
+		log.error("Stream error:", String(error));
+		response.status(500).end();
+	});
+
+	stream.pipe(response);
+}
