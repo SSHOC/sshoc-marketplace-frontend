f50c42a7f615ed0ea433590162f3edef89da5872
Stefan Probst <stefanprobst@posteo.de>
2026-02-19 10:53:05 +0100

    feat: make workflow history page public (#321)
    closes #317

--- components/item/WorkflowControls.tsx
+++ components/item/WorkflowControls.tsx
@@ -1,7 +1,12 @@
+import { useTranslations } from "next-intl";
 import type { ReactNode } from "react";
 
+import { LinkButton } from "@/components/common/LinkButton";
 import { ItemControls } from "@/components/item/ItemControls";
+import css from "@/components/item/ItemControls.module.css";
 import type { Workflow } from "@/data/sshoc/api/workflow";
+import { useCurrentUser } from "@/data/sshoc/hooks/auth";
+import { itemRoutes } from "@/lib/core/navigation/item-routes";
 
 export interface WorkflowControlsProps {
 	persistentId: Workflow["persistentId"];
@@ -9,6 +14,28 @@ export interface WorkflowControlsProps {
 
 export function WorkflowControls(props: WorkflowControlsProps): ReactNode {
 	const { persistentId } = props;
+
+	const t = useTranslations();
+
+	const currentUser = useCurrentUser();
+
+	/**
+	 * Workflow version history is publicly accessible to unauthenticated users, to be able to view
+	 * the workflow version a handle pid resolves to.
+	 */
+	if (currentUser.data == null) {
+		return (
+			<div className={css["container"]}>
+				<LinkButton
+					href={itemRoutes.ItemHistoryPage("workflow")({ persistentId })}
+					color="secondary"
+					size="xs"
+				>
+					{t("common.controls.history")}
+				</LinkButton>
+			</div>
+		);
+	}
 
 	return <ItemControls category="workflow" persistentId={persistentId} />;
 }
